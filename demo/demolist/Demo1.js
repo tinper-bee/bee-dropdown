43033d40e750f39321e3d988371fbc9668bd88b6
Boyuzhou <386607913@qq.com>
2016-12-20 16:08:59 +0800

    init

--- demo/demolist/Demo1.js
+++ demo/demolist/Demo1.js
@@ -10,28 +10,13 @@ class Demo1 extends Component {
         return (
             <div className="demoPadding">
             <Dropdown title="下拉菜单">
-            <Dropdown.Item disabled eventKey="A">下拉一</Dropdown.Item>
-            <Dropdown.Item eventKey="B">下拉2</Dropdown.Item>
-            <Dropdown.Item eventKey="C">下拉3</Dropdown.Item>
-            <Dropdown.Item divider eventKey="D">下拉3</Dropdown.Item>
-            <Dropdown.Item eventKey="E">下拉4</Dropdown.Item>
+                <Dropdown.Item disabled eventKey="A">下拉一</Dropdown.Item>
+                <Dropdown.Item eventKey="B">下拉2</Dropdown.Item>
+                <Dropdown.Item eventKey="C">下拉3</Dropdown.Item>
+                <Dropdown.Item divider eventKey="D">下拉3</Dropdown.Item>
+                <Dropdown.Item eventKey="E">下拉4</Dropdown.Item>
             </Dropdown>
-            <Dropdown title="上拉菜单" dropup>
-            <Dropdown.Item disabled eventKey="A">下拉一</Dropdown.Item>
-            <Dropdown.Item eventKey="B">下拉2</Dropdown.Item>
-            <Dropdown.Item eventKey="C">下拉3</Dropdown.Item>
-            <Dropdown.Item divider eventKey="D">下拉3</Dropdown.Item>
-            <Dropdown.Item eventKey="E">下拉4</Dropdown.Item>
-            </Dropdown>
-            <Dropdown title="不可点击的下拉菜单" disabled colors="danger">
-            <Dropdown.Item disabled>下拉一</Dropdown.Item>
-            <Dropdown.Item>下拉2</Dropdown.Item>
-            <Dropdown.Item>下拉3</Dropdown.Item>
-            <Dropdown.Item divider>下拉3</Dropdown.Item>
-            <Dropdown.Item>下拉4</Dropdown.Item>
-            </Dropdown>
-            <Button colors='primary'></Button>
-    
+
             </div>
         )
     }
