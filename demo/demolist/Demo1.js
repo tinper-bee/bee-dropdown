a8a42e34bd1ea1d617dce2b2ad3fbad2f5c39f17
zhaoyuu <zhaoyuu@yonyou.com>
2016-12-26 09:40:50 +0800

    format code

--- demo/demolist/Demo1.js
+++ demo/demolist/Demo1.js
@@ -6,17 +6,17 @@
  */
 
 class Demo1 extends Component {
-    render () {
+    render() {
         return (
             <Dropdown title="默认下拉" activeKey="B">
-            <Dropdown.Item eventKey="A">下拉一</Dropdown.Item>
-            <Dropdown.Item eventKey="B">下拉2</Dropdown.Item>
-            <Dropdown.Item eventKey="C">下拉3</Dropdown.Item>
-            <Dropdown.Item eventKey="D">下拉4</Dropdown.Item>
-            <Dropdown.Item divider>分割线</Dropdown.Item>
-            <Dropdown.Item eventKey="A" disabled>不可点击</Dropdown.Item>
-            <Dropdown.Item eventKey="B">活跃的</Dropdown.Item>
-            <Dropdown.Item href="https://www.tinper.org">锚点</Dropdown.Item>
+                <Dropdown.Item eventKey="A">下拉一</Dropdown.Item>
+                <Dropdown.Item eventKey="B">下拉2</Dropdown.Item>
+                <Dropdown.Item eventKey="C">下拉3</Dropdown.Item>
+                <Dropdown.Item eventKey="D">下拉4</Dropdown.Item>
+                <Dropdown.Item divider>分割线</Dropdown.Item>
+                <Dropdown.Item eventKey="A" disabled>不可点击</Dropdown.Item>
+                <Dropdown.Item eventKey="B">活跃的</Dropdown.Item>
+                <Dropdown.Item href="https://www.tinper.org">锚点</Dropdown.Item>
             </Dropdown>
         )
     }
